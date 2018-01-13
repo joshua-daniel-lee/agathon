eceadde45be5384d78faabccb35d6272331baa7a
Joshua Lee <jdl93@nau.edu>
2018-01-13 11:04:12 -1000

    setting up the router

---
+++ breadbox/src/containers/landing.js
@@ -0,0 +1,18 @@
+/*jshint esversion: 6 */
+import React, { Component } from 'react';
+
+class Landing extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div className="header">
+        <h1>Hello World Header</h1>
+      </div>
+    );
+  }
+}
+
+export default Landing;
--- breadbox/src/index.js
+++ breadbox/src/index.js
@@ -2,5 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Router>
+    <div className="container">
+      <Route exact path="/" component={App} />
+    </div>
+  </Router>,
+  document.getElementById('root')
+);
