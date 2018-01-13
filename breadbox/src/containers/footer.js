0d59f2e3de9efdc1645ea2b931961cec342321a7
Joshua Lee <jdl93@nau.edu>
2018-01-13 10:43:48 -1000

    bug fixing with npm

---
+++ breadbox/src/containers/footer.js
@@ -0,0 +1,18 @@
+/*jshint esversion: 6 */
+import React, { Component } from 'react';
+
+class Footer extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div className="header">
+        <h1>Hello World Footer</h1>
+      </div>
+    );
+  }
+}
+
+export default Footer;
---
+++ breadbox/src/containers/header.js
@@ -0,0 +1,18 @@
+/*jshint esversion: 6 */
+import React, { Component } from 'react';
+
+class Header extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div className="header">
+        <h1>Hello World Header</h1>
+      </div>
+    );
+  }
+}
+
+export default Header;
