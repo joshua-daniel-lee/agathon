1a73a44790a6ac1e027043ee9a83ad3b5450e817
Joshua Lee <jdl93@nau.edu>
2018-01-14 09:12:06 -1000

    landing site

--- breadbox/src/containers/landing.js
+++ breadbox/src/containers/landing.js
@@ -1,5 +1,5 @@
 /*jshint esversion: 6 */
-import React, { Component } from 'react';
+import React, { Component, Jumbotron, Button } from 'react';
 import Header from './header.js';
 
 class Landing extends Component {
@@ -11,6 +11,22 @@ class Landing extends Component {
     return (
       <div>
         <Header />
+        <div className="container">
+          <div className="jumbotron">
+            <h1 class="display-3 logo font">breadbox</h1>
+            <h2 class="display-3">The fastest way to FSMA compliance</h2>
+            <p class="lead">
+              breadbox lets you secure, share and edit all your files from anywhere.
+            </p>
+            <hr class="my-4" />
+            <a class="btn btn-primary btn-lg" href="#" role="button">
+              Learn More About FSMA
+            </a>
+          </div>
+          <div className="features container">
+            <h1>info</h1>
+          </div>
+        </div>
       </div>
     );
   }
